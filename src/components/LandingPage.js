f19a70ae90d6c432153d914cb7b0f0ef8ec92e1c
Sayan Chatterjee <sayanc354@gmail.com>
2024-09-02 19:43:15 +0530

    Initial commit

---
+++ src/components/LandingPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <h1>Build Your PC</h1>
+      {/* Form to select components */}
+    </div>
+  );
+};
+
+export default LandingPage;
